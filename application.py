73564c3b852fa4764804d13e4cbe25b078d00740
Roman Serazhiev <romanserazhiev@me.com>
2020-07-29 15:48:37 +0200

    added pages for catch jobs and K15t

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from datetime import datetime
+import random
 
 app = Flask(__name__)
 
@@ -17,6 +18,18 @@ def works():
 def atlassianVirtualSummit():
   return render_template("atlassian-virtual-summit.html")
 
+@app.route("/k15t/support")
+def k15tSupport():
+  title = 'Support Engineer at K15t'
+  photos = {
+    'k15t_benjy.jpg': 'Benjy in the office next to my table, March 2018',
+    'k15t_benjy_newoffice.jpg': 'Benjy is checking out the new office space, November 2017',
+    'k15t_roman_stairs.jpg': 'Me on the stairs in the K15t office building, June 2017',
+    'k15t_work_place.jpg': 'My work place, July 2015'
+  }
+  photos_sample = random.sample(photos.items(), 2)
+  return render_template("k15t_support.html", title=title, photos_sample=photos_sample)
+
 @app.route("/podcasts/around-design/ep1")
 def podcastAroundDesignEp1():
   title = 'Graphical design for the first episode of the \u00ABPodcast Around Design\u00BB'
@@ -26,7 +39,22 @@ def podcastAroundDesignEp1():
 def podcastAroundDesignCover():
   title = 'Graphical design for the \u00ABPodcast Around Design\u00BB cover'
   return render_template("podcast_cover.html", title=title)
+
+@app.route("/catch/support-engineer")
+def catchSupportEngineer():
+  title = 'Support Engineer at Catch Software'
+  return render_template("catch_support.html", title=title)
+
+@app.route("/catch/consultant")
+def catchImplementationConsultant():
+  title = 'Implementation Consultant at Catch Software'
+  return render_template("catch_consultant.html", title=title)
   
+@app.route("/catch/tester")
+def catchTester():
+  title = 'Test Analyst at Catch Software'
+  return render_template("catch_tester.html", title=title)
+
 
 # setting the static folder to root of the website when running in DEBUG (on local machine)
 # in PROD static resources should be handled by Apache or Nginx
